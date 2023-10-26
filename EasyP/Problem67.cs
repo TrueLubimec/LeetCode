7f2bcf2f803a413f3f5c21320f659eb7830882a1
Lubimec_ <dowslf@gmail.com>
2023-10-26 22:26:28 +0500

    Problem69

--- EasyP/Problem67.cs
+++ EasyP/Problem67.cs
@@ -1,25 +1,25 @@
 ﻿namespace LeetCode;
 
-//                                      70% DONE ну там реально бред, я такие длинные числа буду годами дебагать
-public class Solution {
-    public string AddBinary(string a, string b) {
-        int ex = 0;
-        int powerA = a.Length - 1;
-        int powerB = b.Length - 1;
-        for (int i = 0; i < a.Length; i++)
-        {
-            int f = (int)Math.Pow(2, powerA);
-            int s = int.Parse(char.ToString(a[i]));
-            ex += f * s;
-            powerA--;
-        }
-        for (int i = 0; i < b.Length; i++)
-        {
-            int f = (int)Math.Pow(2, powerB);
-            int s = int.Parse(char.ToString(b[i]));
-            ex += f * s;
-            powerB--;
-        }
-        return Convert.ToString(ex, 2);
-    }
-}
+// //                                     70% DONE ну там реально бред, я такие длинные числа буду годами дебагать
+// public class Solution {
+//     public string AddBinary(string a, string b) {
+//         int ex = 0;
+//         int powerA = a.Length - 1;
+//         int powerB = b.Length - 1;
+//         for (int i = 0; i < a.Length; i++)
+//         {
+//             int f = (int)Math.Pow(2, powerA);
+//             int s = int.Parse(char.ToString(a[i]));
+//             ex += f * s;
+//             powerA--;
+//         }
+//         for (int i = 0; i < b.Length; i++)
+//         {
+//             int f = (int)Math.Pow(2, powerB);
+//             int s = int.Parse(char.ToString(b[i]));
+//             ex += f * s;
+//             powerB--;
+//         }
+//         return Convert.ToString(ex, 2);
+//     }
+// }
---
+++ EasyP/Problem69.cs
@@ -0,0 +1,18 @@
+﻿// namespace LeetCode;
+
+// //                                          DONE     украл, но разобрался 
+// public class Solution {
+//     public int MySqrt(int x) {
+//         int first=0,last=x;
+//         while (first<=last) {
+// 			long mid=(first+last)/2,temp=mid*mid;
+//             if (x==temp) 
+//                 return (int)mid;
+//             else if (temp>x) 
+//                 last=(int)(mid-1);
+//             else 
+//                 first=(int)(mid+1);
+//         }
+//         return last;
+//     }
+// }
